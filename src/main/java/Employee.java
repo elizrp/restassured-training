e335b2d8da3e512fed87bb6b19d501fb8f332a23
Elizabeth Pavlova <elizabet.pavlova@estafet.com>
2022-11-02 09:33:06 +0200

    Fix lombok annotations

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,17 +1,14 @@
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Employee {
 
     private String name;
     private int salary;
     private int age;
-
-    public Employee(String name, int salary, int age) {
-        this.name = name;
-        this.salary = salary;
-        this.age = age;
-    }
+    
 }
